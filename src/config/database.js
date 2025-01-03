09ebd9f740bbd88babb9a2e829fea1c2560e9af7
yangxuan <1130180692@qq.com>
2025-01-03 14:34:21 +0800

    feat: 修改数据库密码

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const isProd = process.env.NODE_ENV === "production" ? true : false;
 // 配置数据库连接信息
-const sequelize = new Sequelize(isProd ? "marry_db_prod" : "marry_db_dev", "root", "cy4u1314", {
+const sequelize = new Sequelize(isProd ? "marry_db_prod" : "marry_db_dev", "root", "Cy4u1314.", {
   host: "localhost",
   dialect: "mysql",
 });
